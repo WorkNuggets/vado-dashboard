4fbfce549df22ba6fcf75f8f29e5d0ace0e19676
Spenser Wellen <spenserwellen@gmail.com>
2025-03-05 22:48:56 -0800

    updated themecontext to support default system settings

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import type React from "react";
-import { createContext, useState, useContext, useEffect } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 type Theme = "light" | "dark";
 
@@ -17,11 +16,16 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
   const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
-    // This code will only run on the client side
     const savedTheme = localStorage.getItem("theme") as Theme | null;
-    const initialTheme = savedTheme || "light"; // Default to light theme
 
-    setTheme(initialTheme);
+    if (!savedTheme) {
+      const userPrefersDark =
+        window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+      setTheme(userPrefersDark ? "dark" : "light");
+    } else {
+      setTheme(savedTheme);
+    }
+
     setIsInitialized(true);
   }, []);
 
